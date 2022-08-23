667dbe20199200ee69639384f9c50b27a4cdffd5
yuemillo <yc.gd1688@gmail.com>
2022-08-24 01:11:26 +0800

    Update 2022-08-23T17:11:24.230Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,4 +3,9 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true
+});
 //# sourceMappingURL=all.js.map
